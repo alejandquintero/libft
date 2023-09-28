f938de3480825391183f5409c412d7727ee2bdfc
Alejandro Quintero Espinoza <aquinter@c3r6s3.42madrid.com>
2023-09-29 00:20:44 +0200

    Correction in memory

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:28:22 by aquinter          #+#    #+#             */
-/*   Updated: 2023/09/27 19:49:09 by aquinter         ###   ########.fr       */
+/*   Updated: 2023/09/29 00:12:43 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*head;
 	t_list	*aux;
 	t_list	*node;
-	char	*str;
+	void	*str;
 
 	head = NULL;
 	if (lst != NULL)
@@ -26,14 +26,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		while (aux != NULL)
 		{
 			str = f(aux->content);
-			node = malloc(sizeof(t_list));
-			if (!str || !node)
+			node = ft_lstnew(str);
+			if (!node)
 			{
+				del(str);
 				ft_lstclear(&head, del);
 				return (NULL);
 			}
-			node->content = str;
-			node->next = NULL;
 			ft_lstadd_back(&head, node);
 			aux = aux->next;
 		}
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:33:06 by aquinter          #+#    #+#             */
-/*   Updated: 2023/09/23 20:09:20 by aquinter         ###   ########.fr       */
+/*   Updated: 2023/09/28 23:58:14 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	s2 = malloc((len + 1) * sizeof(char));
 	if (!s2)
 		return (NULL);
